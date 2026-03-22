a4ac5b77b983082e83c51c311ad8bb1530fde5b2
VZ807 <victorvader807@gmail.com>
2026-03-22 14:45:00 -0400

    Cereal

--- src/main/java/Cereal.java
+++ src/main/java/Cereal.java
@@ -5,18 +5,42 @@
  * @author – Class period
  *
  */
-public class Cereal
-{
-    // private instance variables to hold info about a cereal from the data set
-    // the name, calories, fiber, carbohydrates, and cups
+public class Cereal {
+  // private instance variables to hold info about a cereal from the data set
+  // the name, calories, fiber, carbohydrates, and cups
+  private String name;
+  private double calories;
+  private double fiber;
+  private double cups;
+  private double carbs;
 
-    // constructor that creates a cereal object
+  // constructor that creates a cereal object
+  public Cereal(String name, double calories, double fiber, double carbs,
+                double cups) {
+    this.name = name;
+    this.calories = calories;
+    this.fiber = fiber;
+    this.carbs = carbs;
+    this.cups = cups;
+  }
 
-    // accessor methods for each instance variable
+  // accessor methods for each instance variable
+  String getName() { return name; }
 
-    /* toString method returns the values stored in the instance variables
-       concatenated in a sentence starting with "Cereal: " */
+  double getCalories() { return calories; }
 
+  double getFiber() { return fiber; }
+
+  double getCarbs() { return carbs; }
+
+  double getCups() { return cups; }
+
+  /*
+   * toString method returns the values stored in the instance variables
+   * concatenated in a sentence starting with "Cereal: "
+   */
+  public String toString() {
+    return "Cereal:" + name + "," + calories + "," + fiber + "," + carbs + "," +
+        cups;
+  }
 }
-
-
--- src/main/java/CerealRunner.java
+++ src/main/java/CerealRunner.java
@@ -1,53 +1,52 @@
-import java.util.Scanner;
-import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class CerealRunner
-{
-    // create a private instance variable to store an ArrayList of Cereal objects
+public class CerealRunner {
+  // create a private instance variable to store an ArrayList of Cereal objects
+  private ArrayList<Cereal> cereals;
 
-    public CerealRunner(String fileName)
-    {
-        // instantiate the ArrayList
+  public CerealRunner(String fileName) {
+    // instantiate the ArrayList
+    cereals = new ArrayList<Cereal>();
 
-        try
-        {
-            FileReader fileRdr = new FileReader(fileName);
-            Scanner scan = new Scanner(fileRdr);
-            while(scan.hasNext())
-            {
-                String myStr = scan.nextLine();
+    try {
+      FileReader fileRdr = new FileReader(fileName);
+      Scanner scan = new Scanner(fileRdr);
+      while (scan.hasNext()) {
+        String myStr = scan.nextLine();
 
-                // use the split method to parse the data into an array of
-                //   String objects
+        // use the split method to parse the data into an array of
+        // String objects
+        String[] uData = myStr.split(",");
+        // go through each String array element and save it
+        // into the appropriate variable which will be used to
+        // create a Cereal object
+        //
+        // The name, calories, fiber, carbohydrates, and cups
+        cereals.add(new Cereal(uData[0], Double.parseDouble(uData[1]),
+                               Double.parseDouble(uData[2]),
+                               Double.parseDouble(uData[3]),
+                               Double.parseDouble(uData[4])));
 
-                // go through each String array element and save it
-                //   into the appropriate variable which will be used to
-                //   create a Cereal object
+        // create a new Cereal object, and add it to the ArrayList
+      }
+      scan.close();
 
-                // create a new Cereal object, and add it to the ArrayList
-
-            }
-            scan.close();
-
-
-        } catch (FileNotFoundException e)
-        {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        int numCereals = cereals.size();
-        System.out.println(numCereals + " records created.");
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
 
-    public static void main(String[] args)
-    {
-        String fileName = "src/data/cerealSubset.csv";
-        CerealRunner cr = new CerealRunner(fileName);
-    }
+    int numCereals = cereals.size();
+    System.out.println(numCereals + " records created.");
+  }
 
+  public static void main(String[] args) {
+    String fileName = "src/data/cerealSubset.csv";
+    CerealRunner cr = new CerealRunner(fileName);
+  }
 }
-
